bbc23c05bff4d229693786f1c5efae231f6df7c5
3went2 <webb.png@gmail.com>
2018-08-07 18:25:07 +0800

    update testing

--- test/test.js
+++ test/test.js
@@ -7,47 +7,47 @@ import delay from 'delay';
 afterEach(unmount);
 
 describe('ScrollView component', () => {
-	test('should children work', () => {
+	test('should `children` work', () => {
 		const text = 'hello world';
 		const wrapper = mount(<ScrollView>{text}</ScrollView>);
 		expect(wrapper.getDOMNode().textContent).toBe(text);
 	});
 
-	test('should onScroll work', () => {
-		const text = 'hello world';
+	test('should `overflowY` be `auto`', () => {
+		const wrapper = mount(<ScrollView />);
+		expect(wrapper.getDOMNode().style.overflowY).toBe('auto');
+	});
+
+	test('should `disabled` work', () => {
+		const wrapper = mount(<ScrollView disabled />);
+		expect(wrapper.getDOMNode().style.overflowY).toBe('hidden');
+	});
+
+	test('should `isHorizontal` work', () => {
+		const wrapper = mount(<ScrollView isHorizontal />);
+		expect(wrapper.getDOMNode().style.overflowX).toBe('auto');
+	});
+
+	test('should `onScroll` work', () => {
 		const ref = createRef();
 		const handleScroll = jest.fn();
-		mount(
-			<ScrollView innerRef={ref} onScroll={handleScroll}>
-				{text}
-			</ScrollView>,
-		);
+		mount(<ScrollView innerRef={ref} onScroll={handleScroll} />);
 		simulate(ref).scroll(10);
 		expect(handleScroll).toHaveBeenCalledTimes(1);
 	});
 
-	test('should onScrollStart work', () => {
-		const text = 'hello world';
+	test('should `onScrollStart` work', () => {
 		const ref = createRef();
 		const handleScrollStart = jest.fn();
-		mount(
-			<ScrollView innerRef={ref} onScrollStart={handleScrollStart}>
-				{text}
-			</ScrollView>,
-		);
+		mount(<ScrollView innerRef={ref} onScroll={handleScrollStart} />);
 		simulate(ref).scroll(10);
 		expect(handleScrollStart).toHaveBeenCalledTimes(1);
 	});
 
-	test('should onScrollEnd work', async () => {
-		const text = 'hello world';
+	test('should `onScrollEnd` work', async () => {
 		const ref = createRef();
 		const handleScrollEnd = jest.fn();
-		mount(
-			<ScrollView innerRef={ref} onScrollEnd={handleScrollEnd}>
-				{text}
-			</ScrollView>,
-		);
+		mount(<ScrollView innerRef={ref} onScroll={handleScrollEnd} />);
 		simulate(ref).scroll(10);
 		await delay(200);
 		expect(handleScrollEnd).toHaveBeenCalledTimes(1);
