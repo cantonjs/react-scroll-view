912ec5dbd4c6e2bed3bec64f481ea1e0e37c2300
3went2 <webb.png@gmail.com>
2018-09-21 14:59:04 +0800

    expose RefreshControlObserver component

--- src/index.js
+++ src/index.js
@@ -3,3 +3,4 @@ export ScrollObserver from './components/ScrollObserver';
 export StickySection from './components/StickySection';
 export Fixed from './components/Fixed';
 export RefreshControl from './components/RefreshControl';
+export RefreshControlObserver from './components/RefreshControlObserver';
